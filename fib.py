1ff5faf1281fb288f86f282c9099a3462b177961
LoriD7 <37154765+LoriD7@users.noreply.github.com>
2020-01-29 22:31:41 -0800

    unable to resolve indentation error.

--- fib.py
+++ fib.py
@@ -1,11 +1,12 @@
-def fib(n):
+def fib(n): """set parameter, n. The following lines are the start of the sequence."""
+
     a = 0
     b = 1
 
     print(a)
     print(b)
 
-    for i in range(2, n):
+    for i in range(2, n): """Will loop until end of the range.The program will shift through the sequences."""
          c = a + b
          a = b
          b = c
